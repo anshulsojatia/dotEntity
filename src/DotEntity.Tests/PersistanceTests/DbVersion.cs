1931bd791fefd9c78a292d25a320982847479129
anshul <anshul@apexol.com>
2017-07-18 20:16:47 +0530

    relocated persistance tests

---
+++ src/DotEntity.Tests/PersistanceTests/DbVersion.cs
@@ -0,0 +1,31 @@
+﻿using DotEntity.Tests.Data;
+using DotEntity.Versioning;
+
+namespace DotEntity.Tests.PersistanceTests
+{
+    public class DbVersion : IDatabaseVersion
+    {
+        public string VersionKey => "TestKey";
+        public void Upgrade(IDotEntityTransaction transaction)
+        {
+            DotEntity.Database.CreateTables(new[]
+            {
+                typeof(Product),
+                typeof(Category),
+                typeof(ProductCategory)
+            }, transaction);
+
+            DotEntity.Database.CreateConstraint(Relation.Create<Product, ProductCategory>("Id", "ProductId"), transaction);
+            DotEntity.Database.CreateConstraint(Relation.Create<Category, ProductCategory>("Id", "CategoryId"), transaction);
+        }
+
+        public void Downgrade(IDotEntityTransaction transaction)
+        {
+            DotEntity.Database.DropConstraint(Relation.Create<Product, ProductCategory>("Id", "ProductId"), transaction);
+            DotEntity.Database.DropConstraint(Relation.Create<Category, ProductCategory>("Id", "CategoryId"), transaction);
+            DotEntity.Database.DropTable<ProductCategory>(transaction);
+            DotEntity.Database.DropTable<Product>(transaction);
+            DotEntity.Database.DropTable<Category>(transaction);
+        }
+    }
+}
--- src/DotEntity.Tests/PersistanceTests/SqlDatabasePersistanceTests.cs
+++ src/DotEntity.Tests/PersistanceTests/SqlDatabasePersistanceTests.cs
@@ -14,7 +14,7 @@ using DotEntity.SqlServer;
 using DotEntity.Tests.Data;
 using DotEntity.Versioning;
 
-namespace DotEntity.Tests
+namespace DotEntity.Tests.PersistanceTests
 {
     [TestFixture]
     public class SqlDatabasePersistanceTests
@@ -318,31 +318,6 @@ namespace DotEntity.Tests
 
             var deletedProduct = EntitySet<Product>.Where(x => x.Id == id).SelectSingle();
             Assert.AreEqual(null, deletedProduct);
-        }
-        public class DbVersion : IDatabaseVersion
-        {
-            public string VersionKey => "TestKey";
-            public void Upgrade(IDotEntityTransaction transaction)
-            {
-                DotEntity.Database.CreateTables(new[]
-                {
-                    typeof(Product),
-                    typeof(Category),
-                    typeof(ProductCategory)
-                }, transaction);
-
-                DotEntity.Database.CreateConstraint(Relation.Create<Product, ProductCategory>("Id", "ProductId"), transaction);
-                DotEntity.Database.CreateConstraint(Relation.Create<Category, ProductCategory>("Id", "CategoryId"), transaction);
-            }
-
-            public void Downgrade(IDotEntityTransaction transaction)
-            {
-                DotEntity.Database.DropConstraint(Relation.Create<Product, ProductCategory>("Id", "ProductId"), transaction);
-                DotEntity.Database.DropConstraint(Relation.Create<Category, ProductCategory>("Id", "CategoryId"), transaction);
-                DotEntity.Database.DropTable<ProductCategory>(transaction);
-                DotEntity.Database.DropTable<Product>(transaction);
-                DotEntity.Database.DropTable<Category>(transaction);
-            }
         }
     }
 }
