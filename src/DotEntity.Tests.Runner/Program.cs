d4a501f853018d7dc43a898120b6b456eb550cfa
anshul <anshul@apexol.com>
2017-07-24 16:44:47 +0530

    added test runner for nunit tests

---
+++ src/DotEntity.Tests.Runner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using NUnitLite;
+
+namespace DotEntity.Tests.Runner
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var assembly = typeof(DotEntityTest).GetTypeInfo().Assembly;
+            return new AutoRun(assembly).Execute(args);
+        }
+    }
+}
