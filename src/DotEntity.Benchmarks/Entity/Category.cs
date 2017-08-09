9eb2407000c77b95b62a12c2046b266078a2d827
anshul <anshul@apexol.com>
2017-08-09 12:20:34 +0530

    added benchmark project

---
+++ src/DotEntity.Benchmarks/Entity/Category.cs
@@ -0,0 +1,19 @@
+﻿// #region Author Information
+// // Category.cs
+// // 
+// // (c) Apexol Technologies. All Rights Reserved.
+// // 
+// #endregion
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DotEntity.Benchmarks.Entity
+{
+    public class Category
+    {
+        public string CategoryName { get; set; }
+
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ src/DotEntity.Benchmarks/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Running;
+
+namespace DotEntity.Benchmarks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("=======================================================================================================================", ConsoleColor.Green);
+            WriteLine("========================================  dotEntity Benchmarks  =======================================================", ConsoleColor.Green);
+            WriteLine("=======================================================================================================================", ConsoleColor.Green);
+#if DEBUG
+            Write($"You are running benchmarks in debugging mode. This might not give accurate results. Continue (Y/N)?", ConsoleColor.Red);
+            var key = Console.ReadKey();
+            WriteLine();
+            if (key.Key != ConsoleKey.Y)
+            {
+                Write("Good byee!...");
+                Console.ReadKey();
+                return;
+            }
+#endif
+            var benchmarks = new List<Benchmark>();
+            var benchmarkAssemblyTypes = typeof(Program).Assembly.DefinedTypes.Where(t => t.IsSubclassOf(typeof(BenchmarkSetup)));
+
+            foreach (var type in benchmarkAssemblyTypes)
+            {
+                var typeBenchmarks = BenchmarkConverter.TypeToBenchmarks(type);
+                benchmarks.AddRange(typeBenchmarks);
+            }
+
+            BenchmarkRunner.Run(benchmarks.ToArray(), null);
+            Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static void WriteLine(string message = "", ConsoleColor color = ConsoleColor.White)
+        {
+            Write(message, color);
+            Console.WriteLine();
+        }
+        static void Write(string message, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
