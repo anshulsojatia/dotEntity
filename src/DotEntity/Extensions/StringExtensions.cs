6ac59d243d0cd4b24956fc2536d601cde191d9b4
anshul <anshul@apexol.com>
2017-08-13 15:21:31 +0530

    missed file from last commit

---
+++ src/DotEntity/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace DotEntity.Extensions
+{
+    public static class StringExtensions
+    {
+        private static readonly string[] ExcludeFromEnclosure = { "INFORMATION_SCHEMA.TABLES", "1" };
+
+        public static string ToEnclosed(this string str)
+        {
+            if (string.IsNullOrEmpty(str) || ExcludeFromEnclosure.Contains(str))
+                return str;
+            var alias = "";
+            var actualStr = str;
+            if (str.Contains(".")) //a case of aliasing
+            {
+                var parts = str.Split('.');
+                alias = parts[0] + ".";
+                actualStr = parts[1];
+            }
+            return alias + DotEntityDb.Provider.SafeEnclose(actualStr);
+        }
+    }
+}
