a1507b91f75c7578283348d696abd0324f12df89
anshul <anshul@apexol.com>
2019-12-06 17:35:15 +0530

    Config settings for deployment server

--- src/DotEntity.Tests/DotEntityTest.cs
+++ src/DotEntity.Tests/DotEntityTest.cs
@@ -10,6 +10,7 @@ namespace DotEntity.Tests
     public class DotEntityTest
     {
         protected bool IsAppVeyor;
+        protected bool IsDeploymentServer;
         protected string MySqlConnectionString;
         protected string MsSqlConnectionString;
         protected string SqliteConnectionString;
@@ -24,12 +25,16 @@ namespace DotEntity.Tests
 
 #endif
             IsAppVeyor = Environment.GetEnvironmentVariable("appveyor") == "true";
+            IsDeploymentServer = Environment.GetEnvironmentVariable("env.dotentity_deployment") == "true";
             MySqlConnectionString = this.IsAppVeyor
                 ? @"Server=127.0.0.1;Uid=root;Pwd=Password12!;Database=mytest;"
                 : @"Server=127.0.0.1;Uid=root;Pwd=admin;Database=unittest;";
 
-            MsSqlConnectionString = IsAppVeyor ? @"Server=(local)\SQL2016;Database=master;User ID=sa;Password=Password12!"
-                : @"Data Source=.;Initial Catalog=unittest_db;Integrated Security=True;Persist Security Info=False;User ID=iis_user;Password=iis_user";
+            MsSqlConnectionString = IsAppVeyor
+                ? @"Server=(local)\SQL2016;Database=master;User ID=sa;Password=Password12!"
+                : IsDeploymentServer
+                    ? @"Data Source=.\SqlExpress;Initial Catalog=unittest_db;Integrated Security=True;"
+                    : @"Data Source=.;Initial Catalog=unittest_db;Integrated Security=True;Persist Security Info=False;User ID=iis_user;Password=iis_user";
 
             
             SqliteConnectionString = $"Data Source={_sqliteFile};";
